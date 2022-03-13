7fbbc52531d67451338fc915afb5da110b3efe7d
Rob Hoelz <rob@hoelz.ro>
2022-03-13 15:19:56 -0500

    Have most operations wrap operations against actual history table
    The virtual table is mostly a proxy for the real history table, so
    wire up the readonly operations as such

--- histdb.lua
+++ histdb.lua
@@ -1,3 +1,5 @@
+local sqlite3 = require 'lsqlite3'
+
 local mod = {
   name = 'h',
   disconnect = function() end,
@@ -17,7 +19,9 @@ function mod.connect(db, args)
   )
   ]]
 
-  return {}
+  return {
+    db = sqlite3.open_ptr(db:get_ptr()),
+  }
 end
 
 function mod.best_index()
@@ -27,25 +31,40 @@ function mod.best_index()
 end
 
 function mod.open(vtab)
-  return {}
+  -- XXX shouldn't this be in filter?
+  local stmt = vtab.db:prepare 'SELECT rowid, * FROM history'
+  return {
+    stmt = stmt,
+    last_status = sqlite3.ROW,
+  }
 end
 
 function mod.close(cursor)
+  cursor.stmt:finalize()
 end
 
 function mod.filter(cursor)
+  return mod.next(cursor)
 end
 
 function mod.eof(cursor)
+  return cursor.last_status ~= sqlite3.ROW and cursor.last_status ~= sqlite3.BUSY
 end
 
 function mod.column(cursor, n)
+  return cursor.stmt:get_value(n + 1)
 end
 
 function mod.next(cursor)
+  local status = cursor.stmt:step()
+  cursor.last_status = status
+  if status == sqlite3.ERROR or status == sqlite3.MISUSE then
+    return nil, 'error stepping'
+  end
 end
 
 function mod.rowid(cursor)
+  return cursor.stmt:get_value(0)
 end
 
 return mod
