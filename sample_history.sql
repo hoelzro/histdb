3e66d17bf0d079690544357cb3c69de21f9678fe
Rob Hoelz <rob@hoelz.ro>
2025-06-09 07:03:00 -0500

    Set NULL session_id for missing timestamp

--- sample_history.sql
+++ sample_history.sql
@@ -11,9 +11,9 @@ CREATE TABLE history (
 );
 
 INSERT INTO history (hostname, session_id, timestamp, history_id, cwd, entry, duration, exit_status) VALUES
-  ('host1', '1', 1640995200, 1, '/home/rob', 'vim file1', 10, 0),
-  ('host1', '1', 1640995260, 2, '/home/rob', 'ls -l', 2, 0),
-  ('host1', '2', 1641081600, 3, '/home/rob/project', 'git status', 3, 0),
-  ('host1', '2', 1641168000, 4, '/home/rob', 'vim file2', 20, 0),
-  ('host2', '3', 1641254400, 5, '/home/rob', 'echo test', 1, 0),
-  ('host2', '4', NULL, 6, '/home/rob', 'incomplete', 1, 1);
+  ('host1', '017e12ef-9c00-7a64-ae73-cffc1360299c', 1640995200, 1, '/home/rob', 'vim file1', 10, 0),
+  ('host1', '017e12ef-9c00-7a64-ae73-cffc1360299c', 1640995260, 2, '/home/rob', 'ls -l', 2, 0),
+  ('host1', '017e1815-f800-715d-88dc-0590f94e9710', 1641081600, 3, '/home/rob/project', 'git status', 3, 0),
+  ('host1', '017e1815-f800-715d-88dc-0590f94e9710', 1641168000, 4, '/home/rob', 'vim file2', 20, 0),
+  ('host2', '017e2262-b000-78d9-bed8-c98696ada0be', 1641254400, 5, '/home/rob', 'echo test', 1, 0),
+  ('host2', NULL, NULL, 6, '/home/rob', 'incomplete', 1, 1);
